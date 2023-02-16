fe1676580c6e71a7ee784df18f789e674f8b6c70
MDM988 <mmould@star.sr.bham.ac.uk>
2023-02-16 22:30:51 +0000

    0.0.2

---
+++ catus/__init__.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.size': 10,
+    'figure.figsize': (5, 5),
+    'figure.titlesize': 'large',
+    'axes.titlesize': 'large',
+    'axes.labelsize': 'medium',
+    'xtick.top': True,
+    'xtick.major.size': 5,
+    'xtick.minor.size': 3,
+    'xtick.minor.visible': True,
+    'xtick.direction': 'out',
+    'xtick.labelsize': 'medium',
+    'ytick.right': True,
+    'ytick.major.size': 5,
+    'ytick.minor.size': 3,
+    'ytick.minor.visible': True,
+    'ytick.direction': 'out',
+    'ytick.labelsize': 'medium',
+    'legend.frameon': True,
+    'legend.framealpha': 1,
+    'legend.fontsize': 'medium',
+    'lines.linewidth': 1,
+    })
+    
+cmap = plt.get_cmap('tab10')
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+name = 'catus'
+version = '0.0.2' # current 0.0.2
+
+with open('README.md' ,'r') as f:
+    long_description = f.read().strip()
+
+setup(
+    name=name,
+    version=version,
+    description=name,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/mdmould/{name}',
+    author='Matthew Mould',
+    author_email='mattdmould@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+    )
